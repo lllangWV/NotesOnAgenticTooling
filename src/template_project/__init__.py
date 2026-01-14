0c8df86cae1cf4fe6f88c8d579b3a698d8d02416
Logan Lang <lllangWV@gmail.com>
2026-01-14 15:52:56 -0500

    Remove template project scaffolding
    Strip out the Python project template files to repurpose this
    repository for notes and scripts on agentic tooling.

--- src/template_project/__init__.py
+++
@@ -1,2 +0,0 @@
-# Copyright (c) YKK AP Technologies Lab (NA) Inc. Proprietary and Confidential.
-# See LICENSE for use restrictions.
--- tests/test_1.py
+++
@@ -1,6 +0,0 @@
-# Copyright (c) YKK AP Technologies Lab (NA) Inc. Proprietary and Confidential.
-# See LICENSE for use restrictions.
-
-
-def test_1() -> None:
-    assert 1 == 1
